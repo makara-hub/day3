9b384e2329638688c28b349dc8f5e8411d24dfc7
makara-hub <143202322+makara-hub@users.noreply.github.com>
2025-02-10 22:47:38 +0530

    Create vowels.java

---
+++ vowels.java
@@ -0,0 +1,17 @@
+public class Main {
+public static void main(String[] args)
+{
+String str = "ram";
+int count = 0;
+ for (int i = 0; i < str.length(); i++) {
+if (str.charAt(i) == 'a' ||str.charAt(i) == 'e'|| str.charAt(i) == 'i'||str.charAt(i) == 'o'||str.charAt(i) == 'u') {
+                count++;
+}
+}
+ System.out.println(
+            "Total no of vowels in string are: " + count);
+    int rem=str.length()-count;
+    System.out.println(
+            "remaining values are " + rem);
+}
+}
